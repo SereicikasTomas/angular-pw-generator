43cc3414307870f6c2c67f1913c405c0dc015b02
sereicikas.t <sereicikas.t@gmail.com>
2026-03-18 18:26:38 +0100

    add injectSpeedInsights

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -7,6 +8,11 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
+
+injectSpeedInsights({
+  framework: 'angular',
+  route: window.location.pathname
+});
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
